bb5be45c043ddc197d2499990b8554539779a177
Tibor Vass <teabee89@gmail.com>
2018-04-12 15:09:44 -0700

    Do not leak server process
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- example/run.sh
+++ example/run.sh
@@ -7,8 +7,15 @@ importpath=github.com/tiborvass/servercover
 ./clean.sh
 go get "$importpath"/cmd/servercover
 $GOPATH/bin/servercover -socket cover.sock -o server -coverpkg "$importpath"/example/program/... "$importpath"/example/program/server
-./server example.sock &
-
 go build -o client "$importpath"/example/program/client
+
+./server example.sock &
 go test -coverprofile cover.out "$importpath"/example/tests -- "$PWD"/client "$PWD"/example.sock "$PWD"/cover.sock
-go tool cover -html=cover.out
+kill -9 %1
+
+go tool cover -func=cover.out
+# HTML output works too
+#go tool cover -html=cover.out
+
+echo
+echo "You may clean up with clean.sh"
